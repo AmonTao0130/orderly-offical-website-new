16a72cc6381e5df48786f169879fb4aa43b75739
Jame Zhan <jame@orderly.network>
2023-11-24 17:17:07 +0800

    add fontSize

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -18,7 +20,34 @@ export default {
       "2xl": "1920px",
       // => @media (min-width: 1920px) { ... }
     },
+    fontSize: {
+      xs: "12px",
+      sm: "14px",
+      base: "16px",
+      lg: "18px",
+      xl: "20px",
+      "2xl": "24px",
+      "3xl": "36px",
+      "4xl": "40px",
+      "5xl": "56px",
+      "6xl": "64px",
+      "7xl": "72px",
+      "8xl": "80px",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      const baseCSS = {
+        color: "white",
+        backgroundColor: "rgb(51, 51, 51)",
+        fontFamily: "system-ui",
+        margin: 0,
+      };
+      addBase({
+        html: baseCSS,
+        body: baseCSS,
+      });
+    }),
+  ],
 };
