8e1364c125ee8aaf5342c19de650b6809620d48e
Jame Zhan <jame@orderly.network>
2023-11-27 14:49:39 +0800

    update build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  // output: "server",
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
