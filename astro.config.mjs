cf9f91e6f8c8d5c2cc577820a3705a3abf990ad6
Jame Zhan <jame@orderly.network>
2023-11-29 22:22:51 +0800

    update output to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-// import node from "@astrojs/node";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react(), tailwind()],
-  // output: "server",
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
   /** 允许ip地址访问 */
   server: { host: true },
 });
