e0df0e5dd21f43eb175fa20b18be73a4881a2a54
Jame Zhan <jame@orderly.network>
2023-12-04 22:49:41 +0800

    add blog header

---
+++ src/layouts/blog/HeaderBg.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { PropsWithClassName } from "@/types";
+import Bg from "./imgs/header-bg.png";
+import { cn } from "@/utils";
+
+const HeaderBg: React.FC<PropsWithClassName> = (props) => {
+  return (
+    <img
+      className={cn(
+        "absolute w-full mix-blend-screen object-cover",
+        /** 375 */
+        "h-[360px]",
+        /** 768 */
+        "md:h-[400px]"
+      )}
+      src={Bg.src}
+    />
+  );
+};
+
+export default HeaderBg;
---
+++ src/layouts/blog/HeaderText.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import type { PropsWithClassName } from "@/types";
+import Content from "@/components/Content";
+import { cn } from "@/utils";
+
+const HeaderText: React.FC<PropsWithClassName> = (props) => {
+  return (
+    <Content>
+      <div className="text-center">
+        <div
+          className={cn(
+            /** 375 */
+            "text-[32px] leading-[32.64px] mt-[88px]",
+            /** 768 */
+            "md:text-[40px] md:leading-[40.8px] md:mt-[99px]",
+            /** 1024 */
+            "lg:text-6xl lg:leading-[65.28px] lg:mt-[71px]",
+            /** 1440 */
+            "xl:",
+            /** 1440 */
+            "2xl:"
+          )}
+        >
+          Orderly Network Blog
+        </div>
+        <div
+          className={cn(
+            /** 375 */
+            "text-sm leading-[21px] mt-[8px]",
+            /** 768 */
+            "md:text-base md:leading-[24px]",
+            /** 1024 */
+            "lg:text-xl lg:leading-[30px]",
+            /** 1440 */
+            "xl:",
+            /** 1440 */
+            "2xl:"
+          )}
+        ></div>
+        Learn about Orderly’s technology, research,
+        <br className="hidden md:inline lg:hidden" /> and
+        <br className="md:hidden" /> latest developments.
+      </div>
+    </Content>
+  );
+};
+
+export default HeaderText;
