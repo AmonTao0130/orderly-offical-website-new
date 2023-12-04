39dc648caa9c2ee335acaa8c0149ed69c251abf5
Jame Zhan <jame@orderly.network>
2023-12-04 17:40:28 +0800

    add UseCases imgs

--- src/layouts/main/UseCases/index.tsx
+++ src/layouts/main/UseCases/index.tsx
@@ -5,7 +5,13 @@ import { useStore } from "@nanostores/react";
 import Content from "@/components/Content";
 import { cn } from "@/utils";
 import Button from "@/components/Button";
-import UseCasesBg from "./usecases-bg.png";
+import Brokers from "./imgs/Brokers.png";
+import Aggregators from "./imgs/Aggregators.png";
+import WalletsCustodians from "./imgs/WalletsCustodians.png";
+import HFT from "./imgs/HFT.png";
+import TradingBots from "./imgs/TradingBots.png";
+import Hedging from "./imgs/Hedging.png";
+import GamesdApps from "./imgs/GamesdApps.png";
 import type { PropsWithClassName } from "@/types";
 
 const data = [
@@ -13,41 +19,49 @@ const data = [
     title: "Brokers",
     content:
       "Create your own Spot or Perps DEX effortlessly on NEAR, and Perps DEX on EVM. Benefit from our professional trading infrastructure and deep liquidity.",
+    img: Brokers.src,
   },
   {
     title: "Aggregators",
     content:
       "Tap into the strength of a shared orderbook and deep liquidity. Directly connect to the entirety of Orderly's ecosystem liquidity by creating your own front-end.",
+    img: Aggregators.src,
   },
   {
     title: "Wallets & Custodians",
     content:
       "Integrate with Orderly for optimal swap rates. Create a custom widget and bring our ecosystem to your users.",
+    img: WalletsCustodians.src,
   },
   {
     title: "HFT",
     content:
       "HFT Use our API and enjoy a CEX-level trading experience thanks to our low-latency orderbook.",
+    img: HFT.src,
   },
   {
     title: "Trading Bots",
     content:
       "Connect to our orderbook to offer the industry's finest Spot & Perps rates. Benefit from SL/limit orders, gas-free transactions, and fully customizable fees.",
+    img: TradingBots.src,
   },
   {
     title: "Hedging",
     content:
       "Safeguard your positions on other exchanges by using our orderbook for effective hedging strategies.",
+    img: Hedging.src,
   },
   {
     title: "Games / dApps",
     content:
       "Enhance your in-app experience and token utility with our in-game Swaps using the built-in widget. Retain users by eliminating third-party redirects",
+    img: GamesdApps.src,
   },
 ];
 
 const titles = data.map((item) => item.title);
 const contents = data.map((item) => item.content);
+const imgs = data.map((item) => item.img);
 
 const UseCases: React.FC<PropsWithClassName> = (props) => {
   const expandKey = useStore(useCasesExpandKey) || 0;
@@ -66,7 +80,7 @@ const UseCases: React.FC<PropsWithClassName> = (props) => {
           "lg:justify-center"
         )}
       >
-        {/* TODO: */}
+        {/* TODO: 移动端不能横向滚动 */}
         {titles.map((title, index) => {
           const expanded = expandKey === index;
           return (
@@ -104,20 +118,24 @@ const UseCases: React.FC<PropsWithClassName> = (props) => {
           /** 375 */
           "p-[32px] mt-[20px]",
           /** 768 */
-          "md:p-[60px] md:mt-[32px]"
+          "md:h-[341px] md:p-[60px] md:mt-[32px]",
+          /** 1024 */
+          "lg:h-[363px]",
+          /** 1440 */
+          "xl:h-[400px]"
         )}
       >
         <img
           className={cn(
-            "absolute right-0 bottom-0 rounded-[24px] mix-blend-screen",
+            "absolute right-0 bottom-0 rounded-[24px] mix-blend-screen h-full",
             /** 375 */
-            "w-[220px]",
-            /** 768 */
-            "md:w-[300px]",
+            "h-[200px]",
             /** 1024 */
-            "lg:w-[500px]"
+            "lg:h-[300px]",
+            /** 1440 */
+            "xl:h-[400px]"
           )}
-          src={UseCasesBg.src}
+          src={imgs[expandKey]}
         />
         {/* <div className="absolute top-0 left-0 z-10 w-full h-full rounded-[24px]"></div> */}
         <div
