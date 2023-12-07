17d8fd839a02a991f4a614cae2800582968b0448
Jame Zhan <jame@orderly.network>
2023-12-07 22:56:34 +0800

    add ecosystem header

---
+++ src/layouts/ecosystem/HeaderBg.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import type { PropsWithClassName } from "@/types";
+import { cn } from "@/utils";
+import left from "./imgs/eco-left.png";
+import right from "./imgs/eco-right.png";
+
+const HeaderBg: React.FC<PropsWithClassName> = (props) => {
+  return (
+    <div
+      // style={{
+      //   background:
+      //     "linear-gradient(180deg, #D9D9D9 91.11%, rgba(217, 217, 217, 0) 101.4%)",
+      // }}
+      className={cn(
+        "absolute top-0 left-0 w-full h-full mix-blend-screen",
+        /** 375 */
+        "",
+        /** 768 */
+        "md:",
+        /** 1024 */
+        "lg:",
+        /** 1440 */
+        "xl:",
+        /** 1440 */
+        "2xl:"
+      )}
+    >
+      <img
+        className={cn(
+          "absolute top-[-50px] left-0 overflow-hidden",
+          /** 375 */
+          "h-[270px]",
+          /** 768 */
+          "md:h-[361px]",
+          /** 1024 */
+          "lg:h-[448px]",
+          /** 1440 */
+          "xl:h-[561px]"
+        )}
+        src={left.src}
+      />
+      <img
+        className={cn(
+          "absolute bottom-0 right-0",
+          /** 375 */
+          "h-[227px]",
+          /** 768 */
+          "md:h-[303px]",
+          /** 1024 */
+          "lg:h-[377px]",
+          /** 1440 */
+          "xl:h-[471px]"
+        )}
+        src={right.src}
+      />
+      <div
+        className={cn(
+          "bg-[#2B0089] blur-[200px] rotate-[14.57deg] overflow-hidden",
+          "absolute top-[60px] left-[-570px]",
+          /** 375 */
+          "w-[388px] h-[353px]",
+          /** 768 */
+          "md:",
+          /** 1024 */
+          "w-[645px] h-[586px]",
+          /** 1440 */
+          "xl:h-[471px]",
+          /** 1440 */
+          "2xl:"
+        )}
+      ></div>
+      <div
+        className={cn(
+          "bg-[#2B0089] blur-[200px] rotate-[14.57deg] overflow-hidden",
+          "absolute top-[-57px] right-[-430px]",
+          /** 375 */
+          "w-[476px] h-[432px]",
+          /** 768 */
+          "md:",
+          /** 1024 */
+          "w-[645px] h-[586px]",
+          /** 1440 */
+          "xl:h-[471px]",
+          /** 1440 */
+          "2xl:"
+        )}
+      ></div>
+    </div>
+  );
+  // TODO:添加两边的渐变效果
+
+  // <div
+  //   className={cn(
+  //     "bg-[rgba(90,37,176,0.9)] blur-[150px] rotate-[6.72deg]",
+  //     "absolute top-0 right-[-700px]",
+  //     /** 375 */
+  //     "",
+  //     /** 768 */
+  //     "md:",
+  //     /** 1024 */
+  //     "lg:",
+  //     /** 1440 */
+  //     "xl:w-[939px] lg:h-full",
+  //     /** 1440 */
+  //     "2xl:"
+  //   )}
+  // ></div>
+};
+
+export default HeaderBg;
---
+++ src/layouts/ecosystem/Statistics.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import type { PropsWithClassName } from "@/types";
+import { cn } from "@/utils";
+
+interface StatisticsItemProps {
+  label: string;
+  value: string;
+  border?: boolean;
+}
+
+const StatisticsItem: React.FC<StatisticsItemProps & PropsWithClassName> = (
+  props
+) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-1 flex-col",
+        props.border && "border-r-[1px] border-r-solid border-r-primary-20",
+        /** 375 */
+        "",
+        /** 768 */
+        "md",
+        /** 1024 */
+        "lg",
+        /** 1440 */
+        "xl"
+      )}
+    >
+      <div
+        className={cn(
+          /** 375 */
+          "text-[10px] leading-[10.2px] font-title text-primary-100 ",
+          /** 768 */
+          "md:text-base md:leading-[16.32px]",
+          /** 1024 */
+          "lg:text-xl lg:leading-[20.4px]",
+          /** 1440 */
+          "xl"
+        )}
+      >
+        {props.label}
+      </div>
+      <div
+        style={{
+          background:
+            "linear-gradient(360deg, #9975FF -30.75%, rgba(155, 249, 255, 0) 103.38%), #FFFFFF",
+          backgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          WebkitBackgroundClip: "text",
+        }}
+        className={cn(
+          /** 375 */
+          "text-2xl leading-[24px] font-bold mt-[8px]",
+          /** 768 */
+          "md:text-[32px] md:leading-[32px]",
+          /** 1024 */
+          "lg:text-5xl lg:leading-[56px] lg:mt-[16px]"
+        )}
+      >
+        {props.value}
+      </div>
+    </div>
+  );
+};
+
+const Statistics: React.FC<PropsWithClassName> = (props) => {
+  return (
+    <div
+      style={{
+        background:
+          "linear-gradient(180deg, rgba(38, 18, 73, 0.3) 0%, rgba(111, 69, 184, 0.3) 100%)",
+      }}
+      className={cn(
+        "text-center backdrop-blur-[25px] rounded-[12px]",
+        "p-[28px] pb-[32px]",
+        /** 375 */
+        "mt-[41px]",
+        /** 768 */
+        "md:mt-[72px]",
+        /** 1024 */
+        "lg:mt-[74px] ",
+        /** 1440 */
+        "xl:mt-[74px]"
+      )}
+    >
+      <div
+        className={cn(
+          "flex",
+          /** 375 */
+          " ",
+          /** 768 */
+          "md",
+          /** 1024 */
+          "lg",
+          /** 1440 */
+          "xl"
+        )}
+      >
+        <StatisticsItem label="Total trading volume" value="$256M+" border />
+        <StatisticsItem label="Traders" value="12,646" border />
+        <StatisticsItem label="Ecosystem partners" value="26" />
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
