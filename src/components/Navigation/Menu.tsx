2ff753addd00f73e1268efe6505de85e8a3883d7
Jame Zhan <jame@orderly.network>
2023-12-08 17:45:49 +0800

    add navigation collapse animation

--- src/components/Navigation/Menu.tsx
+++ src/components/Navigation/Menu.tsx
@@ -27,33 +27,33 @@ const Menu: React.FC<MenuProps> = (props) => {
                 )}
               </div>
 
-              {item.children?.length && (
-                <div
-                  className={cn(
-                    "hidden hover:block group-hover:block absolute top-[20px] z-[1]",
-                    item.showHot ? "left-[-4px]" : "left-[-20px] "
-                  )}
-                >
-                  <div className="mt-[16px] rounded-[12px] [background:linear-gradient(180deg,rgba(38,18,73,0.3)_0%,rgba(111,69,184,0.3)_100%)] border-[1px] border-solid border-[rgba(206,125,255,0.5)] backdrop-blur-[15px]">
-                    {item.children.map((child) => {
-                      return (
-                        <a
-                          key={child.url || child.title}
-                          href={child.url}
-                          target={child.target}
-                          className="flex items-center text-base hover:text-primary-100 px-[20px] my-[24px] first:mt-[20px] last:mb-[20px]"
-                        >
-                          {child.title}
-                          {child.isNew && <NewLabel className="ml-[4px]" />}
-                          {child.showArrow && (
-                            <ArrowRightTopIcon size={14} className="ml-[4px]" />
-                          )}
-                        </a>
-                      );
-                    })}
-                  </div>
+              <div
+                className={cn(
+                  "absolute top-[20px] z-[1] overflow-hidden",
+                  "transition-[max-height] duration-300",
+                  "max-h-0 group-hover:max-h-[178px]",
+                  item.showHot ? "left-[-4px]" : "left-[-20px] "
+                )}
+              >
+                <div className="mt-[16px] rounded-[12px] [background:linear-gradient(180deg,rgba(38,18,73,0.3)_0%,rgba(111,69,184,0.3)_100%)] border-[1px] border-solid border-[rgba(206,125,255,0.5)] backdrop-blur-[15px]">
+                  {item.children.map((child) => {
+                    return (
+                      <a
+                        key={child.url || child.title}
+                        href={child.url}
+                        target={child.target}
+                        className="flex items-center text-base hover:text-primary-100 px-[20px] my-[24px] first:mt-[20px] last:mb-[20px]"
+                      >
+                        {child.title}
+                        {child.isNew && <NewLabel className="ml-[4px]" />}
+                        {child.showArrow && (
+                          <ArrowRightTopIcon size={14} className="ml-[4px]" />
+                        )}
+                      </a>
+                    );
+                  })}
                 </div>
-              )}
+              </div>
             </div>
           </div>
         );
