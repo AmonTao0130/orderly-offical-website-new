a47d2b47ebc0ae5bccfea085e2f8ebdd5b85e89d
Jame Zhan <jame@orderly.network>
2023-12-11 14:47:38 +0800

    fix pageIndexData is null

--- src/layouts/blog/BlogList.tsx
+++ src/layouts/blog/BlogList.tsx
@@ -84,7 +84,7 @@ const BlogList: React.FC<BlogListProps & PropsWithClassName> = (props) => {
   return (
     <div>
       <div className={cn("flex flex-wrap mx-[-10px]", props.className)}>
-        {pageIndexData.map((item, index) => {
+        {pageIndexData?.map((item, index) => {
           return <BlogItem key={item.id} {...item} />;
         })}
       </div>
