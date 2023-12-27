fdf40c0bccbc203ca9293d14c5c955de95b4afd5
Jame Zhan <jame@orderly.network>
2023-12-27 14:59:18 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 // import node from "@astrojs/node";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://orderly.network",
   // Enable React to support React JSX components.
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   // output: "hybrid",
   // adapter: node({
   //   mode: "standalone",
   // }),
   /** 允许ip地址访问 */
-  server: { host: true },
+  server: {
+    host: true,
+  },
 });
