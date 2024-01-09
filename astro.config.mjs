3d0dd92c0110b3a00477e10b9937f883dc19c2a0
Jame Zhan <jame@orderly.network>
2024-01-09 22:12:30 +0800

    rename sitemap site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://orderly.network",
+  site: "https://www.orderly.network",
   // Enable React to support React JSX components.
   integrations: [react(), tailwind(), sitemap()],
   // output: "hybrid",
