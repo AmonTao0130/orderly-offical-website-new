b6b5502c0e56e1841051da3b66e65a666d61152b
Jame Zhan <jame@orderly.network>
2024-03-12 13:38:32 +0800

    banner add scroll text

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,24 +1,30 @@
-import React, { useEffect, useMemo, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import CloseIcon from "@/icons/CloseIcon";
 import { useStore } from "@nanostores/react";
 import { bannerVisible, bannerHeight } from "@/store";
 import { useSize } from "@/hooks/useSize";
 import ArrowForwardIcon from "@/icons/ArrowForwardIcon";
+import { cn } from "@/utils";
 
 interface BannerProps {}
+
+const bannerData = [
+  {
+    title: "Earn Underwater NFTs on Orderly & WOOFi campaign!",
+    url: "https://galxe.com/orderlynetwork/campaign/GCcdEt4UAm",
+    moreText: "GET STARTED",
+  },
+  {
+    title: "Earn 100k $LOGX and NFTs on Orderly & LogX campaign!",
+    url: "https://galxe.com/orderlynetwork/campaign/GCYnAt4USf",
+    moreText: "GET STARTED",
+  },
+];
 
 const Banner: React.FC<BannerProps> = (props) => {
   const { width } = useSize();
   const visible = useStore(bannerVisible);
   const ref = useRef<HTMLDivElement>(null);
-
-  const data = useMemo(
-    () => ({
-      title: "Earn Underwater NFTs on Orderly & WOOFi campaign!",
-      url: "https://galxe.com/orderlynetwork/campaign/GCcdEt4UAm",
-    }),
-    []
-  );
 
   // 当窗口大小变化时，计算Banner的高度
   useEffect(() => {
@@ -32,24 +38,40 @@ const Banner: React.FC<BannerProps> = (props) => {
 
   return (
     <div
-      className="relative flex justify-between items-center px-[24px] py-[16px] text-base [background:linear-gradient(270.23deg,#34D4DE_0.04%,#6473FF_50.25%,#AD2BFE_99.64%)] z-10"
+      className="relative flex justify-between items-center px-[24px] py-[16px] text-base [background:linear-gradient(270.23deg,#34D4DE_0.04%,#6473FF_50.25%,#AD2BFE_99.64%)] z-10  overflow-hidden"
       ref={ref}
     >
-      <div className="flex flex-1 items-center flex-wrap leading-[24px] md:justify-center">
-        {/* TODO: 第二行文字左对齐 */}
-        <span className="text-white font-semibold pr-[12px]">{data.title}</span>
-        <a
-          href={data.url}
-          target="_blank"
-          className="inline-flex items-center text-[#8AEFF5] font-bold text-sm"
-        >
-          <div>GET STARTED</div>
-          <ArrowForwardIcon />
-        </a>
+      <div className="flex flex-col justify-center items-center w-full">
+        {bannerData.map((item, index) => {
+          return (
+            <div
+              className={cn(
+                "flex items-center flex-wrap md:justify-center",
+                "transition-all",
+                index === 0
+                  ? "animate-bannerOut opacity-0"
+                  : "animate-bannerIn opacity-0 absolute left-[24px] right-[24px]"
+              )}
+            >
+              {/* TODO: 第二行文字左对齐 */}
+              <span className="text-white font-semibold pr-[12px] text-base leading-[24px]">
+                {item.title}
+              </span>
+              <a
+                href={item.url}
+                target="_blank"
+                className="inline-flex items-center text-[#8AEFF5] font-bold text-sm leading-[24px]"
+              >
+                <div>{item.moreText}</div>
+                <ArrowForwardIcon />
+              </a>
+            </div>
+          );
+        })}
       </div>
 
       <CloseIcon
-        className="cursor-pointer text-white"
+        className="cursor-pointer text-white z-[1]"
         onClick={() => {
           bannerVisible.set(false);
           bannerHeight.set(0);
