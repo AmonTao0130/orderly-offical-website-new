aa77c04755e86c0c004a249ac727ba364b9259c4
Jame Zhan <jame@orderly.network>
2024-03-22 11:28:10 +0800

    fix LaunchApp click area

--- src/components/Navigation/LaunchApp.tsx
+++ src/components/Navigation/LaunchApp.tsx
@@ -6,7 +6,7 @@ const LaunchApp: React.FC<PropsWithClassName> = (props) => {
   return (
     <button
       className={cn(
-        "text-sm leading-[14px] lg:text-base lg:leading-[16px]",
+        "text-sm leading-[14px] lg:text-base lg:leading-[16px] z-10",
         "h-[40px] px-[20px] text-primary-100 rounded-full ml-[16px] select-none",
         "bg-[linear-gradient(270.23deg,#48BDFF_0.04%,_#786CFF_47.76%,#BD00FF_99.64%)]",
         "hover:bg-[linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2)),linear-gradient(270.23deg,#48BDFF_0.04%,_#786CFF_47.76%,#BD00FF_99.64%)]",
