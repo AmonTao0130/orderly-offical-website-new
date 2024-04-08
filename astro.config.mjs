2d68f338ed7ebd84ac4826a0cc1986a30838a715
Jame Zhan <jame@orderly.network>
2024-04-08 14:51:33 +0800

    add comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ export default defineConfig({
   site: "https://orderly.network",
   // Enable React to support React JSX components.
   integrations: [react(), tailwind(), sitemap()],
+  // 默认预渲染为 HTML, 添加 export const prerender = false 到任何应该按需服务器渲染的文件
   output: "hybrid",
-  // output: "server",
   adapter: node({
     mode: "middleware",
   }),
