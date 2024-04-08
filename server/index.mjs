d38c60ec316992e637746152a8f352d1aaace7db
Jame Zhan <jame@orderly.network>
2024-04-08 14:58:41 +0800

    fix /docs proxy

--- server/index.mjs
+++ server/index.mjs
@@ -3,26 +3,38 @@ import { handler as ssrHandler } from "../dist/server/entry.mjs";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
-// 根据 astro.config.mjs 中的 `base` 选项进行更改。
-// 它们应该匹配。默认值为"/"。
+
+// 根据 astro.config.mjs 中的 `base` 选项进行更改，默认值为"/"
 const base = "/";
 app.use(base, express.static("dist/client/"));
 
-// 添加代理转发规则
+const pathFilter = function (path, req) {
+  if (path === "/") {
+    return true;
+  }
+  return false;
+};
+
+// 添加/docs代理转发到/docs/getting-started/what-is-orderly-network，否则/docs会导致
 app.use(
   "/docs",
   createProxyMiddleware({
-    target: "https://orderly.mintlify.dev:443",
+    target:
+      "https://orderly.mintlify.dev/docs/getting-started/what-is-orderly-network",
     changeOrigin: true,
-    headers: {
-      Host: "orderly.mintlify.dev",
-      "X-Forwarded-Host": "orderly.network",
-      "X-Forwarded-Proto": "https",
-    },
+    pathFilter: pathFilter,
+  })
+);
+
+// 添加/docs/*代理转发规则
+app.use(
+  "/docs",
+  createProxyMiddleware({
+    target: "https://orderly.mintlify.dev/docs",
+    changeOrigin: true,
   })
 );
 
 app.use(ssrHandler);
 
-app.listen(8080);
 app.listen(80);
