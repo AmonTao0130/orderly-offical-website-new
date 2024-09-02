62f1361197e4fedc685d84e927aa09366e866ebb
Jame Zhan <jame@orderly.network>
2024-09-02 22:45:20 +0800

    add /api/articles

---
+++ src/pages/api/articles.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+import { getArticles } from "@/strapi/services";
+import type { PublicationState } from "@/strapi/type";
+
+export const GET: APIRoute = async (data) => {
+  const params = data.url.searchParams;
+
+  const res = await getArticles({
+    pagination: {
+      page: params.get("page") ? parseInt(params.get("page")!) : 1,
+    },
+    category: params.get("category")!,
+    publicationState: params.get("publicationState") as PublicationState,
+  });
+
+  return new Response(JSON.stringify(res));
+};
