5b9045853872a6343b719eee658ca95c8f556c76
Jame Zhan <jame@orderly.network>
2024-09-05 19:24:01 +0800

    update banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,24 +1,21 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, forwardRef } from "react";
 import CloseIcon from "@/icons/CloseIcon";
 import { useStore } from "@nanostores/react";
 import { bannerVisible, bannerHeight } from "@/store";
 import { useSize } from "@/hooks/useSize";
 import ArrowForwardIcon from "@/icons/ArrowForwardIcon";
 import { cn } from "@/utils";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import type { PropsWithClassName } from "@/types";
 
-interface BannerProps {}
+type Banner = {
+  title: string;
+  url: string;
+  moreText: string;
+};
 
-const banners = [
-  // {
-  //   title: "Earn Underwater NFTs on Orderly & WOOFi campaign!",
-  //   url: "https://galxe.com/orderlynetwork/campaign/GCcdEt4UAm",
-  //   moreText: "GET STARTED",
-  // },
-  // {
-  //   title: "Check token eligibility for Orderly Network early supporters",
-  //   url: "https://app.orderly.network/first-riders",
-  //   moreText: "LEARN MORE",
-  // },
+const banners: Banner[] = [
   {
     title: "Unlock 50K $ZRO giveaway on Orderly & LayerZero campaign!",
     url: "https://app.galxe.com/quest/orderlynetwork/GC9jhtxnna",
@@ -29,26 +26,64 @@ const banners = [
     url: "https://airdrop.orderly.network",
     moreText: "CLAIM NOW",
   },
-  // {
-  //   title: "Earn Orderly Merits to boost your share of the airdrop!",
-  //   url: "https://app.orderly.network/",
-  //   moreText: "LEARN MORE",
-  // },
-  // {
-  //   title: "Earn 100k $LOGX and NFTs on Orderly & LogX campaign!",
-  //   url: "https://galxe.com/orderlynetwork/campaign/GCYnAt4USf",
-  //   moreText: "GET STARTED",
-  // },
 ];
 
-const Banner: React.FC<BannerProps> = (props) => {
+const BannerText = forwardRef<HTMLDivElement, Banner & PropsWithClassName>(
+  (props, ref) => {
+    const { title, url, moreText, className } = props;
+    return (
+      <div
+        key={title}
+        style={{
+          transform: "translate3d(0, 0, 0)",
+        }}
+        className={cn(
+          "flex-[0_0_100%] min-h-0 w-full py-[16px]",
+          "flex flex-col justify-center items-start",
+          "md:flex-row md:items-center",
+          className
+        )}
+        ref={ref}
+      >
+        {/* TODO: 第二行文字左对齐 */}
+        <span className="text-white font-semibold pr-[12px] text-base leading-[24px]">
+          {title}
+        </span>
+        <a
+          href={url}
+          target="_blank"
+          className={cn(
+            "inline-flex",
+            "items-center text-[#8AEFF5] font-bold text-sm leading-[32px]"
+          )}
+        >
+          <div>{moreText}</div>
+          <ArrowForwardIcon />
+        </a>
+      </div>
+    );
+  }
+);
+
+const Banner: React.FC = () => {
   const { width } = useSize();
   const visible = useStore(bannerVisible);
+  const height = useStore(bannerHeight);
   const ref = useRef<HTMLDivElement>(null);
 
-  // 当窗口大小变化时，计算Banner的高度
+  const [emblaRef] = useEmblaCarousel(
+    {
+      axis: "y",
+      loop: true,
+      duration: 25,
+      slidesToScroll: "auto",
+    },
+    [Autoplay({ playOnInit: true, delay: 4500 })]
+  );
+
+  // 当窗口大小变化时，计算 Banner 的高度
   useEffect(() => {
-    const height = ref.current?.getBoundingClientRect()?.height;
+    const { height } = ref.current?.getBoundingClientRect() || {};
     bannerHeight.set(height! || 0);
   }, [width]);
 
@@ -56,59 +91,27 @@ const Banner: React.FC<BannerProps> = (props) => {
     return null;
   }
 
-  const isAnimate = banners.length > 1;
-
   return (
     <div
       className={cn(
-        "relative flex justify-between items-center gap-x-[16px]",
-        "px-[24px] py-[16px] text-base z-10 overflow-hidden",
+        "flex items-center z-10 gap-x-[16px] px-[24px] text-base",
         "[background:linear-gradient(270.23deg,#34D4DE_0.04%,#6473FF_50.25%,#AD2BFE_99.64%)]"
       )}
-      ref={ref}
     >
-      <div className="flex flex-col justify-center items-center flex-1">
-        {banners.map((item, index) => {
-          return (
-            <div
-              key={item.title}
-              className={cn(
-                "flex flex-wrap flex-col items-start",
-                "md:flex-row md:items-center md:justify-center",
-                "transition-all",
-                isAnimate &&
-                  (index === 0
-                    ? "animate-bannerOut opacity-0"
-                    : "animate-bannerIn opacity-0 absolute left-[24px] right-[24px]"),
-                index == 1 && "mr-[8px]"
-                // isAnimate &&
-                //   cn(
-                //     "opacity-0",
-                //     idx !== 1 && "absolute left-[24px] right-[24px]",
-                //     idx === 1 && "animate-banner1",
-                //     idx === 2 && "animate-banner2",
-                //     idx === 3 && "animate-banner3"
-                //   ),
-              )}
-            >
-              {/* TODO: 第二行文字左对齐 */}
-              <span className="text-white font-semibold pr-[12px] text-base leading-[24px]">
-                {item.title}
-              </span>
-              <a
-                href={item.url}
-                target="_blank"
-                className={cn(
-                  "inline-flex",
-                  "items-center text-[#8AEFF5] font-bold text-sm leading-[32px]"
-                )}
-              >
-                <div>{item.moreText}</div>
-                <ArrowForwardIcon />
-              </a>
-            </div>
-          );
-        })}
+      <div className="relative flex-1">
+        <div className="overflow-hidden flex-1" ref={emblaRef}>
+          <div className="flex flex-col" style={{ height }}>
+            {banners.map((item) => (
+              <BannerText key={item.title} {...item} />
+            ))}
+          </div>
+        </div>
+
+        <BannerText
+          ref={ref}
+          className={cn(height && "absolute invisible")}
+          {...banners[0]}
+        />
       </div>
 
       <CloseIcon
