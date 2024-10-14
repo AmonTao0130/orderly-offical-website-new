c39fe5ff876db8f70b833b5a054c4154f6581702
Jame Zhan <jame@orderly.network>
2024-10-14 17:40:43 +0800

    integrate beehiiv api

--- src/layouts/main/EmailSubscribe/index.tsx
+++ src/layouts/main/EmailSubscribe/index.tsx
@@ -1,35 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@/components/Button";
 import type { PropsWithClassName } from "@/types";
 import { cn } from "@/utils";
+import axios from "axios";
 
 function validateEmail(email: string) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
-
 const EmailSubscribe: React.FC<PropsWithClassName> = (props) => {
   const [value, setValue] = useState("");
-  const [subscribed, setSubscribed] = useState(false);
+  const [subscribed, setSubscribed] = useState(true);
   const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  // 清空输入框时取消订阅状态
+  useEffect(() => {
+    if (!value) {
+      setSubscribed(false);
+    }
+    setErrorMsg("");
+  }, [value]);
 
   const onSubscribe = () => {
-    // 值为空、email 格式错误、已订阅时
-    if (!value || !validateEmail(value) || subscribed) {
+    // email 格式错误
+    if (!value || !validateEmail(value)) {
       console.log("invalid email");
+      setErrorMsg("Please enter a valid email address.");
       return;
     }
 
     setLoading(true);
-    setTimeout(() => {
-      setSubscribed(true);
-      setLoading(false);
-    }, 1000);
+
+    axios
+      .post("/api/subscribe", {
+        // "jame@orderly.network"
+        email: value,
+      })
+      .then((res) => {
+        setSubscribed(true);
+      })
+      .catch((err) => {
+        setErrorMsg("Something went wrong, please try again later.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
-    <div className={cn("text-center", props.className)}>
+    <div className={cn(props.className)}>
       <div
         className={cn(
+          "text-center",
           /** 375 */
           "font-title font-bold text-[22px] leading-[26px]",
           /** 768 */
@@ -43,47 +65,64 @@ const EmailSubscribe: React.FC<PropsWithClassName> = (props) => {
         Stay updated with Orderly
       </div>
 
-      <div
-        className={cn(
-          "flex justify-center",
-          /** 375 */
-          "mt-[16px]",
-          /** 1440 */
-          "xl:mt-[24px]"
-        )}
-      >
-        <input
-          value={value}
-          onChange={(e) => {
-            console.log("onChange", e.target.value);
-            setValue(e.target.value);
-          }}
-          placeholder="Your email"
-          className={cn(
-            "h-[32px] md:h-[40px] w-[180px] md:w-[240px] px-[16px]",
-            "backdrop-blur-[20px]",
-            "bg-[rgba(209,150,255,0.12)]",
-            "border border-[rgba(209,150,255,0.12)] border-r-0",
-            "rounded-l-full",
-            "outline-none",
-            "text-[13px] lg:text-sm text-primary placeholder:text-[rgba(255,255,255,0.16)]"
+      <div className="flex justify-center">
+        <div>
+          <div
+            className={cn(
+              /** 375 */
+              "mt-[16px]",
+              /** 1440 */
+              "xl:mt-[24px]"
+            )}
+          >
+            <input
+              value={value}
+              onChange={(e) => {
+                console.log("onChange", e.target.value);
+                setValue(e.target.value);
+              }}
+              placeholder="Your email"
+              className={cn(
+                "h-[32px] md:h-[40px] w-[180px] md:w-[240px] px-[16px]",
+                "backdrop-blur-[20px]",
+                "bg-[rgba(209,150,255,0.12)]",
+                "border border-[rgba(209,150,255,0.12)] border-r-0",
+                "rounded-l-full",
+                "outline-none",
+                "text-[13px] lg:text-sm text-primary placeholder:text-[rgba(255,255,255,0.16)]",
+                errorMsg && "border-[rgba(255,99,144,1)]"
+              )}
+            />
+            <Button
+              className={cn(
+                "text-[13px] lg:text-sm",
+                "min-w-[97px] lg:min-w-[118px]",
+                "h-[32px] md:h-[40px] lg:h-[40px]",
+                "px-[16px] md:px-[12px]",
+                "rounded-l-none",
+                subscribed &&
+                  "[background:linear-gradient(347.34deg,#1DF6B5_2.55%,#86ED92_88.55%)] hover:![background:linear-gradient(347.34deg,#1DF6B5_2.55%,#86ED92_88.55%)] cursor-default",
+                errorMsg &&
+                  "[background:rgba(255,99,144,1)] hover:[background:rgba(255,99,144,1)] text-primary-36 cursor-default"
+              )}
+              loading={loading}
+              onClick={subscribed ? undefined : onSubscribe}
+            >
+              {subscribed ? "Subscribed" : "Subscribe"}
+            </Button>
+          </div>
+          {errorMsg && (
+            <div
+              className={cn(
+                "flex items-center gap-x-1 mt-[4px]",
+                "text-left text-[rgba(255,99,144,1)]"
+              )}
+            >
+              <div className="w-[6px] h-[6px] rounded-full bg-[rgba(255,99,144,1)]"></div>
+              {errorMsg}
+            </div>
           )}
-        />
-        <Button
-          className={cn(
-            "text-[13px] lg:text-sm",
-            "min-w-[97px] lg:min-w-[118px]",
-            "h-[32px] md:h-[40px] lg:h-[40px]",
-            "px-[16px] md:px-[12px]",
-            "rounded-l-none",
-            subscribed &&
-              "[background:linear-gradient(347.34deg,#1DF6B5_2.55%,#86ED92_88.55%)]"
-          )}
-          loading={loading}
-          onClick={onSubscribe}
-        >
-          Subscribe
-        </Button>
+        </div>
       </div>
     </div>
   );
