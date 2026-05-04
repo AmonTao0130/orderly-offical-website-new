cdb7594888119759cfd2e9e397ecc021d1438571
Jame Zhan <jame@orderly.network>
2026-05-04 16:35:26 +0800

    feat: add security headers and URL rewrites in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,44 @@ const nextConfig: NextConfig = {
   // For static hosting, consider using a serverless function or form service like Formspree
   // output: "export",
   transpilePackages: ["@mui/icons-material", "@mui/material"],
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/docs",
+        destination:
+          "https://orderly.mintlify.dev/docs/getting-started/what-is-orderly-network",
+      },
+      {
+        source: "/docs/:path*",
+        destination: "https://orderly.mintlify.dev/docs/:path*",
+      },
+    ];
+  },
   webpack(config) {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
